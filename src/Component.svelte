f84141b80949d90d572781a738183f995478d343
Leon Weber <l.weber@itemsnet.de>
2023-08-03 16:06:03 +0200

    display errror fix

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,9 +1,9 @@
 <script>
-  import { getContext, onMount } from "svelte";
+  import { getContext} from "svelte";
 
   export let text;
   export let dataProvider;
-  export let filename
+  export let filename;
 
   const { styleable } = getContext("sdk");
   const component = getContext("component");
@@ -22,56 +22,53 @@
 
   // Function to generate the CSV string
   function generateCsvString(data) {
-    const headers = Object.keys(data[0]).filter(key => !excludedColumns.includes(key));
-    const csvRows = data.map(obj => objectToCsvRow(obj, excludedColumns));
+    const headers = Object.keys(data[0]).filter(
+      (key) => !excludedColumns.includes(key)
+    );
+    const csvRows = data.map((obj) => objectToCsvRow(obj, excludedColumns));
 
     // Join headers and rows with line breaks to form the final CSV string
     return [headers.join(","), ...csvRows].join("\n");
   }
-  const excludedColumns = ['_id', '_rev'];
-
-
-  let data = dataProvider?.rows || [];
-  console.log(generateCsvString(data));
+  const excludedColumns = ["_id", "_rev"];
 
   // Function to handle the button click event
   function saveDataToFile() {
+    let data = dataProvider?.rows || [];
     // Create a Blob (Binary Large Object) from the data
-    const blob = new Blob([generateCsvString(data)], { type: "text/plain" });
+    if (data) {
+      const blob = new Blob([generateCsvString(data)], { type: "text/plain" });
 
-    // Create a temporary URL for the blob
-    const url = URL.createObjectURL(blob);
+      // Create a temporary URL for the blob
+      const url = URL.createObjectURL(blob);
 
-    // Create a link element
-    const link = document.createElement("a");
+      // Create a link element
+      const link = document.createElement("a");
 
-    // Set the link's attributes
-    link.href = url;
-    if (!filename){
-      filename = "iot-erp-bridge-datenexport"
+      // Set the link's attributes
+      link.href = url;
+      if (!filename) {
+        filename = "iot-erp-bridge-datenexport";
+      }
+      link.download = filename + ".csv"; // Set the file name
+
+      // Append the link to the DOM (necessary for Firefox)
+      document.body.appendChild(link);
+
+      // Programmatically trigger a click event on the link to start the download
+      link.click();
+
+      // Clean up by removing the link and revoking the URL
+      link.remove();
+      URL.revokeObjectURL(url);
     }
-    link.download = filename + ".csv"; // Set the file name
-
-    // Append the link to the DOM (necessary for Firefox)
-    document.body.appendChild(link);
-
-    // Programmatically trigger a click event on the link to start the download
-    link.click();
-
-    // Clean up by removing the link and revoking the URL
-    link.remove();
-    URL.revokeObjectURL(url);
   }
 
-  // This function will run when the component is mounted
-  onMount(() => {
-    // You can perform additional setup or logic here if needed
-  });
 </script>
 
 <div use:styleable={$component.styles}>
   <div>
-    <button on:click={saveDataToFile}>Save Data</button>
+    <button on:click={saveDataToFile}>{text}</button>
   </div>
 </div>
 
